9f8b72652a79de5b465f2c5b826ea16850462f5d
Samuel García <38510593+Samuel-GC@users.noreply.github.com>
2025-04-09 16:40:29 -0500

    cambios

--- dashboard-spy/src/App.js
+++ dashboard-spy/src/App.js
@@ -71,11 +71,11 @@ function App() {
         // { url: 'http://35.212.121.135:8001', userId: "Gloria Velasco", etf: "SPY" },
         // { url: 'http://35.212.49.216:8001', userId: "Karin Granda", etf: "SPY" },
 
-        // { url: 'http://35.212.91.124:8001', userId: "Jackie Andren", etf: "SPY" },
+         { url: 'http://35.212.91.124:8001', userId: "Jackie Andren", etf: "SPY" },
         // { url: 'http://35.212.79.208:8001', userId: "Raul Gutierrez ", etf: "SPY" },
          { url: 'http://35.212.13.140:8001', userId: "Gerardo Yupari", etf: "SPY" },
         // { url: 'http://35.212.7.60:8001', userId: "Guillermo Berastain" ,etf:"SPY" },
-          { url: 'http://35.212.31.6:8001', userId: "Augusto Vidaurre" ,etf:"SPY" },
+         // { url: 'http://35.212.31.6:8001', userId: "Augusto Vidaurre" ,etf:"SPY" },
 // 	{ url: 'http://35.212.36.94:8001', userId: "David Fuchang" ,etf:"SPY" },
 // 	{ url: 'http://35.212.43.136:8001', userId: "Augusto Reyna" ,etf:"SPY" },
 // { url: 'http://35.212.33.64:8001', userId: "Antonio Garcia" ,etf:"SPY" },
--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -64,7 +64,7 @@ function App() {
       const urls = [
  
         { url: 'http://35.212.31.6:8000', userId: "Augusto Vidaurre" ,etf:"QQQ" },
-        { url: 'http://35.212.126.197:8000', userId: "Geraldo Arosemena" ,etf:"QQQ" },
+       // { url: 'http://35.212.126.197:8000', userId: "Geraldo Arosemena" ,etf:"QQQ" },
         { url: 'http://35.212.13.140:8000', userId: "Gerardo Yupari" ,etf:"QQQ" },
         { url: 'http://35.212.46.199:8000', userId: "Giancarlo Marchesi" ,etf:"QQQ" },
         { url: 'http://35.212.7.60:8000', userId: "Guillermo Berastain" ,etf:"QQQ" },
@@ -73,7 +73,7 @@ function App() {
         { url: 'http://35.212.44.4:8000', userId: "Orlando Marchesi" ,etf:"QQQ" } ,
 
 
-        { url: 'http://35.212.10.69:8000', userId: "Andres Sotomayor", etf: "QQQ" },
+        { url: 'http://35.212.72.211:8000', userId: "Andres Sotomayor", etf: "QQQ" },
         // { url: 'http://35.212.36.94:8000', userId: "David Fuchang" ,etf:"QQQ" },
  
         
